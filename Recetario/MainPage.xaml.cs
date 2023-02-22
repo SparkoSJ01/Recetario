39ddd98c175528890133274422c70f1f82e9fc2b
SparkoSJ01 <jsantamaria29543-21@ccpm.edu.mx>
2023-02-22 00:09:17 -0600

    appRecetarioVer3

---
+++ Recetario/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Recetario.views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Recetario
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Desayunos_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Desayunos());
+        }
+
+        private async void ComidaMexicana_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComidaMexicana());
+        }
+
+        private async void Cocteles_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cocteles());
+        }
+
+        private async void ComidaVegana_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ComidaVegana());
+        }
+
+        private async void Postres_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Postres());
+        }
+
+        private async void PescadosMariscos_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PescadosMariscos());
+        }
+
+        private async void Pasta_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pasta());
+        }
+
+        private async void AcercaDe_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AcercaDe());
+        }
+    }
+}
---
+++ Recetario/SeedRecetario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recetario
+{
+    class SeedRecetario
+    {
+        //public String dominio = "https://phpseedpprogmov.000webhostapp.com/";
+        public String dominio = "http://10.0.13.6:8080/progmovil/";
+
+    }
+}
